ccc13790481c3e487976f627d43793303591c3e2
Chi Cao Minh <chi.caominh@gmail.com>
2022-01-02 19:07:31 -0800

    slf4j-nop 1.7.32 (#18)

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("com.github.ajalt.clikt:clikt:3.2.0")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:${Versions.KOTEST}")
     testImplementation("io.kotest:kotest-assertions-core-jvm:${Versions.KOTEST}")
-    testRuntimeOnly("org.slf4j:slf4j-nop:1.7.30")
+    testRuntimeOnly("org.slf4j:slf4j-nop:1.7.32")
 }
 
 repositories {
